8c5516b980f3ad7b4a6b6a7e69fd7800c815c1f4
github username <github email address>
2018-06-23 19:42:33 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |category, attribute_hash|
+    attribute_hash.each do |attribute, names_array|
+      names_array.each do |name|
+        if new_hash == {} || new_hash.has_key?(name) == false
+          new_hash[name] = {}
+          new_hash[name][category] = [attribute.to_s]
+        elsif new_hash[name].has_key?(category)
+          new_hash[name][category] << attribute.to_s
+        elsif new_hash.has_key?(name) && new_hash[name].has_key?(category) == false
+          new_hash[name][category] = [attribute.to_s]
+        end
+      end
+    end
+  end
+  new_hash
 end
