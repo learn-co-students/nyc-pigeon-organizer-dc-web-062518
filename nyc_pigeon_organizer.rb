b652034d595ab9fb968e3451d2792bd8fb71909a
finale100 <finale150@gmail.com>
2018-06-24 11:10:09 -0400

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(category, category_hash), pigeon_list|
+    category_hash.each do |category_type, name_array|
+      name_array.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][category] ||= []
+           pigeon_list[name][category] << category_type.to_s
+      end
+    end
+  end
 end
